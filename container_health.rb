b65514801264f21d997eb06ae9441dbe3003db4a
ruijose <ruijosemangas@gmail.com>
2016-04-16 19:50:32 +0000

    added rescure clause

--- container_health.rb
+++ container_health.rb
@@ -7,8 +7,16 @@ CONTAINER_NAME = ARGV[2]
 riemann_client = Riemann::Client.new host: RIEMANN_SERVER, port: 5555, timeout: 5
 
 while true
-  container_id = %x[docker ps | grep #{CONTAINER_NAME} | awk '{print $1}'].chop
-  is_running = system("docker inspect -f {{.State.Running}} " << container_id)
+
+  begin
+    riemann_client['true']
+  rescue Riemann::Client::TcpSocket::Error
+    riemann_client = Riemann::Client.new host: RIEMANN_SERVER, port: 5555, timeout: 5
+    p "reconnecting..."
+  end
+
+  container_id = %x[sudo docker inspect --format '{{ .Id }}' #{CONTAINER_NAME}].chop
+  is_running = system("sudo docker inspect -f {{.State.Running}} " << container_id)
   container_state = is_running ? "ok" : "critical"
   metric = is_running ? 1 : 0
 
@@ -16,10 +24,11 @@ while true
     host: HOSTNAME,
     service: "docker container health",
     state: container_state,
-    metric: metric
+    metric: metric,
+    tags: ["docker"]
   }
 
   p "Event sent. Container state: #{container_state}"
 
-  sleep(120)
+  sleep(2)
 end
