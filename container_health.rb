b11593644355dbb47fdaea238281dc9239cdbc67
ruijose <ruijosemangas@gmail.com>
2016-04-18 11:36:10 +0000

    Bug with value returned from system func solved.

--- container_health.rb
+++ container_health.rb
@@ -16,16 +16,16 @@ while true
   end
 
   container_id = %x[sudo docker inspect --format '{{ .Id }}' #{CONTAINER_NAME}].chop
-  is_running = system("sudo docker inspect -f {{.State.Running}} " << container_id)
-  container_state = is_running ? "ok" : "critical"
-  metric = is_running ? 1 : 0
+  is_running = %x[sudo docker inspect -f {{.State.Running}} #{container_id}].chop
+  container_state = is_running == "true" ? "ok" : "critical"
+  metric = is_running == "true" ? 1 : 0
 
   riemann_client << {
     host: HOSTNAME,
     service: "docker container health",
     state: container_state,
     metric: metric,
-    tags: ["docker"]
+    tags: ["docker", "app server"]
   }
 
   p "Event sent. Container state: #{container_state}"
